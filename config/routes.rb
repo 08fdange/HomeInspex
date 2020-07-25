8a3ac2b64e4660a72fb2e6b05fb8b4998bb21d4b
08fdange <08fdange@gmail.com>
2020-07-25 19:40:10 -0400

    minor tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
 
-  resources :clients, only: :show, path: :profile do
+  resource :client, only: :show, path: :profile do
     resources :inspections, only: [:index, :show, :new, :create]
   end
 
